f8dacb039b2fa4bb7060590f623909e16193e6ce
Sergio Wicramachine <wicramachine@gmail.com>
2025-03-16 18:53:56 +0100

    feat : installation de dotenv et gestion des variables de connexion via .env
    - Installation du package dotenv pour charger les variables d'environnement depuis un fichier .env.
    - Modification de la connexion à la base de données pour utiliser les variables d'environnement au lieu de valeurs codées en dur.

--- models/db.js
+++ models/db.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 // CONNEXION A LA BASE DE DONNEES
 const connection = await mysql.createConnection({
-  host: '127.0.0.1',
-  port: 8889,
-  user: 'flutter',
-  password: 'flutter',
-  database: 'flutter_db',
-  charset: 'utf8'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    charset: process.env.DB_CHARSET
 });
 
 module.exports = connection;
